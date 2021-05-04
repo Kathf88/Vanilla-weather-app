d7281caf949dc6008ce4b26fd17906e15c328f2e
Kathf88 <kathf88@gmail.com>
2021-05-04 19:07:33 +0200

    Displaying the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -24,38 +24,63 @@ return `${day} ${hours}:${minutes}`;
 }
 
 
-function displayForecast () {
+function formatDate (timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue","Wed", "Thu", "Fri", "Sat"];
+
+return days [day];
+
+}
+
+
+function displayForecast (response) {
+
+    let forecast =response.data.daily;
+   
     let forecastElement = document.querySelector("#forecast");
 
-    let days = ["Mon","Tue","Wed","Thu"];
 
     let forecastHTML=`<div class="row">`;
-    days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+        if (index <6) {
+
     forecastHTML =
     forecastHTML + 
     `
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">
+                ${formatDate(forecastDay.dt)}</div>
+                
                 <img
-                  src="http://openweathermap.org/img/wn/01d@2x.png"
+                  src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}.png"
                   alt=""
                   width="42"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max"> 18° </span>
-                  <span class="weather-forecast-temperature-min"> 12° </span>
+                  <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+                  <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
                 </div>
               </div>
             
 `;
-
+}
 });
 forecastHTML=forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
 
 }
 
+function getForecast(coordinates) {
 
+let apiKey = "b8fdffaf7eef3ad3a428f00894228f66"
+let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayForecast);
+
+}
 
 
 function displayTemperature(response) {
@@ -84,7 +109,12 @@ function displayTemperature(response) {
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute (
         "alt", response.data.weather[0].description);
-    
+
+getForecast(response.data.coord);
+
+
+
+
 }
 
 function search(city) {
@@ -149,6 +179,6 @@ celciusLink.addEventListener("click", displayCelciusTemperature);
 
 
 
-search ("Ålesund");
+search ("London");
 
-displayForecast();
+
